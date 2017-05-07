ca86a284cb92dafe4e4fb2a988535b99a43888e6
Cristian <1995.cgonzalez.07@gmail.com>
2017-05-07 16:26:15 -0300

    table "questions" added

--- trivia-app/src/main/java/trivia/App.java
+++ trivia-app/src/main/java/trivia/App.java
@@ -10,13 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
-      Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/trivia", "root", "root");
-
+     
+     Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/trivia", "root", "root");
       User u = new User();
-      u.set("username", "Maradona");
-      u.set("password", "messi");
-      u.saveIt();
-
       Base.close();
     }
 }
