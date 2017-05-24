52cdd4b83aa2ec1a16ab48372c206bd771502db0
Sebastian <sebastiansaez_2003@hotmail.com>
2017-05-23 23:22:53 -0300

    DB modified

--- trivia-app/src/migrations/20170507175256_triviaDB.sql
+++ trivia-app/src/migrations/20170507175256_triviaDB.sql
@@ -1,21 +1,34 @@
-CREATE TABLE users (
-  id  int(11) auto_increment PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users(
+  id int(11) auto_increment PRIMARY KEY,
   username VARCHAR(128) NOT NULL,
   password VARCHAR(128) NOT NULL,
-  permissions ENUM('SI','NO') Default 'NO',
+  permissions ENUM('YES','NO') Default 'NO',
+  state	ENUM('ACTIVE','INACTIVE') Default 'INACTIVE',
+  UNIQUE (username),	
   created_at DATETIME,
   updated_at DATETIME
 )ENGINE=InnoDB;
 
-CREATE TABLE questions (
-  tag VARCHAR(128) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS questions(
+  id int(11) auto_increment PRIMARY KEY,
   category 	ENUM('CIENCIAS','DEPORTE','ENTRETENIMIENTO','GEOGRAFIA','HISTORIA'),
-  question VARCHAR(128) NOT NULL,
+  description VARCHAR(128) NOT NULL,
   option1  VARCHAR(20) NOT NULL,
   option2  VARCHAR(20) NOT NULL,
   option3  VARCHAR(20) NOT NULL,
   option4  VARCHAR(20) NOT NULL,
-  correct	ENUM('1','2','3','4'),	
+  created_at DATETIME,
+  updated_at DATETIME
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS games(
+  id int(11) auto_increment PRIMARY KEY,
+  player1_id int(11) NOT NULL,
+  player2_id int(11) NOT NULL,
+  state	ENUM('IN_PROGRESS','FINISHIED'),
+  scorep1 int(11),
+  scorep2 int(11),
+  turn ENUM('1','2'),
   created_at DATETIME,
   updated_at DATETIME
 )ENGINE=InnoDB;
