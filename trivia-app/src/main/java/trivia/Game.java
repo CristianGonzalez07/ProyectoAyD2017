644a29bb44d47a6720b016d85418cc4dc7d0b75e
davidbea <davidbeaa93@gmail.com>
2017-05-25 12:15:47 -0300

    Class 'Game' modified

--- trivia-app/src/main/java/trivia/Game.java
+++ trivia-app/src/main/java/trivia/Game.java
@@ -2,45 +2,56 @@ package trivia;
 
 import org.javalite.activejdbc.Base;
 import org.javalite.activejdbc.Model;
+import trivia.User;
 import trivia.Question;
 import java.util.List;
-import java.util.Scanner;
 import java.util.Random;
 
-public class Game{
+public class Game extends Model{
+	static{
+    	validatePresenceOf("player1_id").message("Please, provide player1");
+  		validatePresenceOf("player2_id").message("Please, provide player2");
+ 	}
 
-	private static int random(int init,int end) {		//retorna un nro aleatorio en el intervalo tomado como parametro(considerando extremos)
+ 	//retorna un nro aleatorio en el intervalo tomado como parametro(considerando extremos)
+	private static int random(int init,int end) {
 		Random  rnd = new Random();
 		return (int)(rnd.nextDouble() * end + init);
 	}
 
-	public static void play(){							//permite al usuario respoder una pregunta	
-		int n = random(1,5); 
-		System.out.println("El resultado de girar la ruleta fue: "+n);
-		String cat = getCat(n);
+	//crea una partida entre 2 usuarios
+	public static void createGame(String user1,String user2){
 		Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/trivia", "root", "root");
-		List<Question> questions =  Question.getQuestionsByCategory(cat);
-		n = random(0,questions.size()-1);
-		System.out.println("el nro de pregunta fue: "+n);
-		showQuestion(questions.get(n));
+		Game g = new Game();
+     	List<User> users = User.where("username = ?",user1);
+      	User u = users.get(0);
+      	g.set("player1_id", u.get("id"));
+      	User u2 = User.findFirst("username = ?",user2);
+      	g.set("player2_id", u2.get("id"));
+      	g.set("state","IN_PROGRESS");
+      	g.set("turn",'1');
+      	g.set("scorep1",0);
+      	g.set("scorep2",0);
+      	g.saveIt();
 		Base.close();
 	}
 
-	private static String getCat(int n){				//retorna el string correspondiente al nro de cat tomado como parametro
+/*	//retorna el string correspondiente al nro de cat tomado como parametroX!
+	private static String getCat(int n){
 		String res = "";
 		switch (n) {
         case 1:
         	res = "CIENCIAS";
         break;
- 
-        case 2: 
+
+        case 2:
         	res = "DEPORTES" ;
         break;
-       
+
        	case 3:
         	res = "ENTRETENIMIENTO";
         break;
-        
+
         case 4:
        		res = "GEOGRAFIA";
         break;
@@ -58,25 +69,14 @@ public class Game{
 		System.out.println("2- "+q.get("option2"));
 		System.out.println("3- "+q.get("option3"));
 		System.out.println("4- "+q.get("option4"));
-		System.out.println("Ingrese su rta: ");
-		String rta = read();
-		if(q.get("correct").equals(rta)){
-			System.out.println("Respuesta correcta");
-		}else{
-			System.out.println("Respuesta incorrecta");
-		}
 	}
 
 	private static Boolean getAnswer(Question q, String r){	//dada una pregunta y una respuesta determina si la rta es correcta o no
-		if(q.get("correct").equals(r)){
+		if(q.get("option1").equals(r)){
 			return true;
 		}else{
 			return false;
 		}
 	}
-
-	private static String read(){												//retorna un string tomado desde el teclado
-		Scanner sc = new Scanner(System.in);
-		return sc.nextLine();
-	}
+	*/
 }
