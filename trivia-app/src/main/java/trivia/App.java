358adf4c4550bd4bef8f61ebac523798e189e928
Cristian <1995.cgonzalez.07@gmail.com>
2017-05-26 19:53:41 -0300

    class 'app' modified

--- trivia-app/src/main/java/trivia/App.java
+++ trivia-app/src/main/java/trivia/App.java
@@ -1,13 +1,47 @@
 package trivia;
-
+import org.javalite.activejdbc.Base;
 import trivia.User;
-import java.util.Scanner;
+import trivia.Question;
+import trivia.Game;
+
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 
 /**
- * Hello Pregunta2
+ * Hello world!
  *
  */
 public class App
 {
-	
+    public static void main( String[] args )
+    {
+      Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/trivia", "root", "root");
+
+    	Map map = new HashMap();
+
+      get("/", (req, res) -> {
+        return new ModelAndView(map, "./views/mainpage.mustache");
+      }, new MustacheTemplateEngine()
+      );
+
+      get("/login", (req, res) -> {
+        return new ModelAndView(map, "./views/login.mustache");
+      }, new MustacheTemplateEngine()
+      );
+
+      post("/login", (request, response) -> {
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/trivia", "root", "root");
+        User usuario1 = User.createIt("username",request.queryParams("txt_username"),"password",request.queryParams("txt_password"));
+        Base.close();
+        response.redirect("/");
+        return null;   
+    });
+  }       
 }
