74c67366886ae690518ffb1947b48a4d25c7ab1a
Cristian <1995.cgonzalez.07@gmail.com>
2017-06-09 12:59:16 -0300

    DB modified

--- trivia-app/src/migrations/20170507175256_triviaDB.sql
+++ trivia-app/src/migrations/20170507175256_triviaDB.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS users(
   id int(11) auto_increment PRIMARY KEY,
   username VARCHAR(128) NOT NULL,
   password VARCHAR(128) NOT NULL,
-  permissions ENUM('YES','NO') Default 'NO',
-  state	ENUM('ACTIVE','INACTIVE') Default 'INACTIVE',
+  permissions ENUM('YES','NO') DEFAULT 'NO',
+  score int(11) NOT NULL DEFAULT 0,
   UNIQUE (username),	
   created_at DATETIME,
   updated_at DATETIME
@@ -11,12 +11,12 @@ CREATE TABLE IF NOT EXISTS users(
 
 CREATE TABLE IF NOT EXISTS questions(
   id int(11) auto_increment PRIMARY KEY,
-  category 	ENUM('CIENCIAS','DEPORTE','ENTRETENIMIENTO','GEOGRAFIA','HISTORIA'),
+  category 	ENUM('CIENCIAS','DEPORTES','ENTRETENIMIENTO','GEOGRAFIA','HISTORIA'),
   description VARCHAR(128) NOT NULL,
-  option1  VARCHAR(20) NOT NULL,
-  option2  VARCHAR(20) NOT NULL,
-  option3  VARCHAR(20) NOT NULL,
-  option4  VARCHAR(20) NOT NULL,
+  option1  VARCHAR(50) NOT NULL,
+  option2  VARCHAR(50) NOT NULL,
+  option3  VARCHAR(50) NOT NULL,
+  option4  VARCHAR(50) NOT NULL,
   created_at DATETIME,
   updated_at DATETIME
 )ENGINE=InnoDB;
@@ -29,6 +29,7 @@ CREATE TABLE IF NOT EXISTS games(
   scorep1 int(11),
   scorep2 int(11),
   turn ENUM('1','2'),
+  round int NOT NULL DEFAULT 1,
   created_at DATETIME,
   updated_at DATETIME
 )ENGINE=InnoDB;
