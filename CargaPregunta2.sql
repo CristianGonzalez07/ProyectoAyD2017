f8d34bf4d7075fd9a65786c3c47e8e4c9f8cdd0e
Cristian <1995.cgonzalez.07@gmail.com>
2017-06-12 18:31:54 -0300

    CargaPregunta2 added

---
+++ CargaPregunta2.sql
@@ -0,0 +1,29 @@
+INSERT INTO users (username,password,permissions,created_at,updated_at) VALUES 
+('DavidBea','pregunta2','YES',NOW(),NOW()),
+('SebaSaez','pregunta2','YES',NOW(),NOW()),
+('CGonzalez','pregunta2','YES',NOW(),NOW());
+
+INSERT INTO questions (category,description,option1,option2,option3,option4,created_at,updated_at) VALUES
+('DEPORTES','¿Cuántos puntos vale un tiro libre encestado en baloncesto?','1','2','3','Depende',NOW(),NOW()),
+('DEPORTES','¿Cuánto dura un partido de fútbol?','90"','45"','75"','80"',NOW(),NOW()),
+('DEPORTES','¿Dónde se inventó el tenis de mesa?','Inglaterra','China','Japón','Corea del Sur',NOW(),NOW()),
+('DEPORTES','Si durante una mano de Póker me tocan 3 cartas del mismo valor, ¿qué jugada tengo?','Trio','Doble Par','Full','Par',NOW(),NOW()),
+('DEPORTES','¿Quién ganó cuatro mundiales consecutivos de Fórmula 1?','Sebastian Vettel','Fernando Alonso','Lewis Hamilton','kimi Raikkonen',NOW(),NOW()),
+('ENTRETENIMIENTO','¿Quién es la mascota de la empresa de videojuegos SEGA?','Sonic','Mario','Pac Man','Ryu',NOW(),NOW()),
+('ENTRETENIMIENTO','¿De qué distrito es Katniss de Los juegos del hambre?','Distrito 12','Distrito 3','Distrito 2','Distrito 9',NOW(),NOW()),
+('ENTRETENIMIENTO','¿Qué grupo cómico dobló al español a los protagonistas de “Shrek”?','Cruz y Raya','Tip & Coll','Martes & 13','Los Morancos',NOW(),NOW()),
+('ENTRETENIMIENTO','¿Quien es el protagonista de la saga "Rocky"?','Silvester Stallone','Jason Statham','Bruce Lee','Arnold Schwarzernegger',NOW(),NOW()),
+('ENTRETENIMIENTO','¿Cuantos premios Oscar consiguio el actor Leonardo DiCaprio?','1','Ninguno','3','2',NOW(),NOW()),
+('CIENCIAS','¿De qué color es la sange de los peces?','Roja','Verde Oscuro','Azul','Marron',NOW(),NOW()),
+('CIENCIAS','¿Por qué los cocodrilos mantienen la boca abierta durante largo rato?','Para calentarse','Para hacer la digestión','Para beber agua','Para esperar una presa',NOW(),NOW()),
+('CIENCIAS','¿Con qué abren las ostras las nutrias de mar?','Con una piedra','Con los dientes','Con un palo','Con las uñas',NOW(),NOW()),
+('CIENCIAS','¿Cuanto de los sig numeros es Capicua?','121','154','1002','450',NOW(),NOW()),
+('CIENCIAS','¿Cuantos dientes tiene un pingüino promedio?','Ninguno','12','20','10',NOW(),NOW()),
+('GEOGRAFIA','¿Con cuántos países limita Argentina?','5','4','6','3',NOW(),NOW()),
+('GEOGRAFIA','¿En qué país está Ushuaia, la ciudad más al sur del mundo?','Argentina','Chile','Sudáfrica','Nueva Zelanda',NOW(),NOW()),
+('GEOGRAFIA','¿A qué país pertenece la isla Mujeres? ','Mexico','Argentina','Colombia','Chile',NOW(),NOW()),
+('GEOGRAFIA','¿Cuál es el idioma oficial de Uruguay?','Español','Uruguayo','Guarani','Español y Uruguayo',NOW(),NOW()),
+('HISTORIA','¿En qué año tuvo lugar el ataque a Pearl Harbor?','1941','1939','1940','1942',NOW(),NOW()),
+('HISTORIA','¿Quién liberó a Argentina, Chile y Perú?','José de San Martín','Simón Bolívar','Ernesto ‘Che’ Guevara','Manual Belgrano',NOW(),NOW()),
+('HISTORIA','¿Qué motivó la rebelión que dio lugar a la Guerra de la Independencia de EEUU?','Impuesto','Corrupción','Represión','Esclavitud',NOW(),NOW()),
+('HISTORIA','¿Qué imperio llegó a conquistar casí toda Europa?','Romano','Cartago','Espartano','Vikingo',NOW(),NOW());
