e8f4cd022d80d64acb798288bf0fde8f016b12d9
Sebastian <sebastiansaez_2003@hotmail.com>
2017-08-28 23:22:19 -0300

    Class 'lib' added

---
+++ trivia-app/src/main/java/trivia/lib.java
@@ -0,0 +1,17 @@
+package trivia;
+import org.javalite.activejdbc.Base;
+import org.javalite.activejdbc.Model;
+import trivia.User;
+import trivia.Question;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import static spark.Spark.*;
+
+public class lib{
+
+}
