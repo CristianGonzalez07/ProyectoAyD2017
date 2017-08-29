45751054ff0362799541ac8a82b2237c51bf29da
Cristian <1995.cgonzalez.07@gmail.com>
2017-08-29 08:38:13 -0300

    test branch 2

--- trivia-app/src/main/java/trivia/Game.java
+++ trivia-app/src/main/java/trivia/Game.java
@@ -5,7 +5,7 @@ import org.javalite.activejdbc.Model;
 import java.util.List;
 
 public class Game extends Model {
-	
+	//-----------------------------------------	
 	static{
     	validatePresenceOf("user").message("Please, provide your username");
   		validatePresenceOf("description").message("Please, provide your password");
