0da54360d76648d991d80812789c43f8de0da6fa
Cristian <1995.cgonzalez.07@gmail.com>
2017-10-19 20:35:02 -0300

    [#151743494] added 'newInvitations' and 'getInvitations' functions

--- trivia-app/src/main/java/trivia/Invitation.java
+++ trivia-app/src/main/java/trivia/Invitation.java
@@ -14,13 +14,12 @@ public class Invitation extends Model{
      * function that creates a user invitation
      * @param origin user origin of the invitation 
      * @param destination user invitation destination 
-     * @param content content of invitation
-     * @return true if the invitation was created correctly, but false
-     * @pre. origin != null, destination != null, content != null.
+     * @return true if the invitation was created correctly, otherwise false
+     * @pre. origin != null, destination != null
      * @post. true is returned if the invitation was created correctly otherwise
      * false.
      */
-	public static boolean createInvitation(String origin , String destination, String content){
+	public static boolean createInvitation(String origin , String destination){
 
 		Invitation n = new Invitation();
 		n.set("origin",origin);
@@ -30,12 +29,40 @@ public class Invitation extends Model{
 		return result;
 	}
 
-	 /** 
-     * 
-     * @param content content of invitation
-     * @return true if the invitation was created correctly, but false
-     * @pre. origin != null, destination != null, content != null.
-     * @post. true is returned if the invitation was created correctly otherwise
-     * false.
+	/** 
+     * function that checks if the user has unaccepted invitations
+     * @param username is the name of the user that you want to know
+     * if you have invitations
+     * @return true if the user has unaccepted invitations 
+     * @pre. username != null.
+     * @post. true is returned if the user has unaccepted invitations 
      */
+     public static boolean newInvitations(String username){
+          long count = Invitation.count("destination= ? ", username);
+          return count!= 0;
+     }
+
+
+     /** 
+     * function that returns the list of the first 3 user names
+     * that invited the given user as a parameter to a game.
+     * @param username is the name of the user who received the
+     * invitation to a game.
+     * @return the list of the first 3 user names
+     * that invited the given user as a parameter to a game.
+     * @pre. username != null.
+     * @post. return the list of the first 3 user names
+     * that invited the given user as a parameter to a game.
+     */
+     public static List<String> getInvitations(String username){
+          List<String> listOfUsers = new List<String>();
+          Invitation invitation = new Invitation();
+          String origin = "";
+          for(int i = 0;i<3;i++){
+               invitation = findFirst("destination = ?",username);
+               origin = invitation.getString("origin");
+               listOfUsers.add(origin);
+          }
+          return listOfUsers;
+     }
 }
