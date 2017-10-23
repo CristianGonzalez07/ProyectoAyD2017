8ac73b0c377c09e5c93cceb591cb9903050c6431
Cristian <1995.cgonzalez.07@gmail.com>
2017-10-23 17:27:16 -0300

    [#152200123] added 'messaging' functions

--- trivia-app/views/websocketDemo.js
+++ trivia-app/views/websocketDemo.js
@@ -3,23 +3,35 @@ var webSocket = new WebSocket("ws://" + location.hostname + ":" + location.port 
 webSocket.onmessage = function (msg) { update(msg); };
 webSocket.onclose = function () { alert("WebSocket connection closed") };
 
-//Send a message if it's not empty, then clear the input field
-function sendMessage() {
+
+function sendOption() {
         webSocket.send(id("option").value);
+}
+
+
+function sendBuild() {
+        webSocket.send("build");
 }
 
 
 //Update the chat-panel, and the list of connected users
 function update(msg) {
     var data = JSON.parse(msg.data);
-    id("question").innerHTML = "";
-    id("options").innerHTML = "";
-    text = "<button id='option' onclick="
-    insert("question", "<h2>" + data.question + "</h2>");
-    insert("options", text+"sendMessage()>" + data.option1 + "</button>");
-    insert("options", text+"sendMessage()>" + data.option2 + "</button>");
-    insert("options", text+"sendMessage()>" + data.option3 + "</button>");
-    insert("options", text+"sendMessage()>" + data.option4 + "</button>");
+
+    if(data.results !== ""){
+        text = "<button onclick="
+        insert("results","<h2>" + data.results + "</h2>")
+        insert("results", text+"sendBuild()>Continuar</button>");
+    }else{
+        id("question").innerHTML = "";
+        id("options").innerHTML = "";
+        text = "<button id='option' onclick="
+        insert("question", "<h2>" + data.question + "</h2>");
+        insert("options", text+"sendOption()>" + data.option1 + "</button>");
+        insert("options", text+"sendOption()>" + data.option2 + "</button>");
+        insert("options", text+"sendOption()>" + data.option3 + "</button>");
+        insert("options", text+"sendOption()>" + data.option4 + "</button>");
+    }
     
 }
 
